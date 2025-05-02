5bd7ba3e7e54a9bea251d6789c456a651fb6b16d
Thanasis Protopapas <protopapas.dim.ath@outlook.com>
2025-05-02 11:21:59 +0300

    Updates

--- assets/index-II_IhFUk.js
+++ assets/index-II_IhFUk.js
@@ -56,7 +56,7 @@ Error generating stack: `+u.message+`
         `}),Z.jsxs("div",{className:`
           w-full bg-white 
           py-8 md:py-[75.5px] lg:py-6 
-          px-6 md:px-[54px] lg:px-6 
+          px-6 md:px-[54px] lg:px-8 
           rounded-t-[25px] md:rounded-b-[25px] 
           md:w-2/3 lg:w-1/2 xl:w-1/3 
           flex flex-col lg:flex-row 
